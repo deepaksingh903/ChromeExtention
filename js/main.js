c296557883bfcd5f7b4284a74264658a3c861876
dpk903 <deepaksingh903@gmail.com>
2017-11-07 01:41:36 +0530

    Update main.js
    bugfix

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function triggerAutoClick()
 
 function autoClick()
 {
-  var doc = document.getElementsByClassName('button Lts($ls-s) Z(0) Whs(nw) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) recsGamepad__button--like')[0].click();
+  var doc = document.getElementsByClassName('button Lts($ls-s) Z(0) Whs(nw) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) recsGamepad__button--like')[0];
 //  var className = 'abc';
 //  var doc = document.getElementById('irc_ra');
 
